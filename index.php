f921cdd74aec2b481c629bc2e931b566d3f8fbe1
Christoph Auer <auer.christoph@outlook.at>
2016-07-29 22:07:17 +0200

    Add checks

--- index.php
+++ index.php
@@ -1,10 +1,36 @@
 <?php
 
-$password = getenv('PASSWORD');
+function isCorrectPassword() {
+    $password = getenv('PASSWORD');
 
-if($password !== false && !empty($password) && (!isset($_GET['password']) || $_GET['password'] != $password)) {
-  header('HTTP/1.0 403 Forbidden', true, 403);
-  exit();
+    if($password === false || empty($password)) {
+        return true;
+    }
+
+    if(!isset($_GET['password']) || $_GET['password'] != $password) {
+        return false;
+    }
+
+    return true;
 }
+
+function isHeaderSet() {
+    if(!isset($_SERVER['HTTP_X_GITLAB_EVENT'])) {
+        return false;
+    }
+
+    if($_SERVER['HTTP_X_GITLAB_EVENT'] != 'Push Hook') {
+        return false;
+    }
+
+    return true;
+}
+
+if(!isHeaderSet() || !isCorrectPassword()) {
+    header('HTTP/1.0 403 Forbidden', true, 403);
+    exit();
+}
+
+file_put_contents('log.txt', print_r($_SERVER, true), FILE_APPEND);
 
 include '../config/config.php';
