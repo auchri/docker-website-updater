2fe92f1aaabf1536e5af2f1a91a5e5487e9a5cba
Christoph Auer <auer.christoph@outlook.at>
2016-07-30 01:02:38 +0200

    Add gitlab url to known hosts

--- start.sh
+++ start.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 [[ ! $PASSWORD ]] && PASSWORD=""
+[[ ! $GITLAB_URL ]] && GITLAB_URL=""
 
 DISABLE_FUNCTIONS="passthru,system,proc_open,popen,show_source"
 KEY_FILE="/var/www/config/private.key"
 
+# Add gitlab to known hosts
+ssh-keyscan "${GITLAB_URL}" >> ~/.ssh/known_hosts
+
+# Add private key to ssh
 chmod 600 "${KEY_FILE}"
 eval "$(ssh-agent)"
 ssh-add "${KEY_FILE}"
