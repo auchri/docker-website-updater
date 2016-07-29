75e4f054600fac0075444e1692828fbd5de7c998
Christoph <auchri@users.noreply.github.com>
2016-07-29 21:24:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $password = getenv('PASSWORD');
 
-if($password !== false && $_GET['password'] != $password) {
+if($password !== false && !empty($password) && (!isset($_GET['password']) || $_GET['password'] != $password)) {
   header('HTTP/1.0 403 Forbidden', true, 403);
   exit();
 }
