318c3a43c3a61854b497061f6189b8ba0edaa861
Christoph <auchri@users.noreply.github.com>
2016-07-30 00:40:27 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,10 @@
 [[ ! $PASSWORD ]] && PASSWORD=""
 
 DISABLE_FUNCTIONS="passthru,system,proc_open,popen,show_source"
+KEY_FILE="/var/www/config/private.key"
 
+chmod 600 "${KEY_FILE}"
 eval "$(ssh-agent)"
-ssh-add /var/www/config/private.key
+ssh-add "${KEY_FILE}"
 
 source /configure.sh
